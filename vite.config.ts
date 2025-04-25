dbed5ac5986f31438fe03fbf2bac7d29e142ef89
Shobhit Rajpurohit <shobhit.21it465@rtu.ac.in>
2025-04-25 19:05:50 +0530

    Fix vite base path for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,20 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './', 
+
   plugins: [react()],
+  
   server: {
     proxy: {
       "/api": {
-        target: "https://medical-backend-16ms.onrender.com", // Your FastAPI backend
+        target: "https://medical-backend-16ms.onrender.com",
         changeOrigin: true,
         secure: false,
       },
     },
   },
+  
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
