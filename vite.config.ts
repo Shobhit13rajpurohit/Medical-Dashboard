b8255aff4d5ad26bf39dde16e5ddf1229725e91b
Shobhit Rajpurohit <shobhit.21it465@rtu.ac.in>
2025-04-25 18:59:15 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:8000", // Your FastAPI backend
+        target: "https://medical-backend-16ms.onrender.com", // Your FastAPI backend
         changeOrigin: true,
         secure: false,
       },
